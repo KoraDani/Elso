5d0513c4de78658a276ea17c9dcad7edd5aebd16
KoraDani <kora.dani@gmail.com>
2020-01-09 20:54:00 +0100

    Hibás

---
+++ Uzemanyag/Uzemanyag/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Uzemanyag
+{
+    class Program
+    {
+        struct uzem
+        {
+           // public DateTime datum;
+            public int ev;
+            public int honap;
+            public int nap;
+            public int benzin;
+            public int dizel;
+        }
+        static void Main(string[] args)
+        {
+            string[] g = File.ReadAllLines("uzemanyag.txt");
+            uzem[] t = new uzem[g.Length];
+            for (int i = 0; i < g.Length; i++)
+            {
+                string[] h = g[i].Split('.');
+                t[i].ev = int.Parse(h[0]);
+                t[i].honap = int.Parse(h[1]);
+                //t[i].nap = Convert.ToInt32(h[2]);
+            }
+            for (int i = 0; i < g.Length; i++)
+            {
+                string[] f = g[i].Split(';');
+                t[i].benzin = Convert.ToInt32(f[0]);
+                t[i].dizel = Convert.ToInt32(f[1]);
+            }
+
+            Console.WriteLine("3.feladat: Változások száma {0}", g.Length);
+
+            int min = Math.Abs(t[0].benzin - t[0].dizel);
+
+            for (int i = 0; i < g.Length; i++)
+            {
+                if (min > Math.Abs(t[i].benzin - t[i].dizel))
+                {
+                    min = Math.Abs(t[i].benzin - t[i].dizel);
+
+                }
+            }
+            int hanyszor = 0;
+            for (int i = 0; i < g.Length; i++)
+            {
+                if (Math.Abs(t[i].benzin - t[i].dizel) == min)
+                {
+                    hanyszor++;
+                }
+            }
+            Console.WriteLine("4.feladat: A legkisseb külömbség: {0}", min);
+            Console.WriteLine("5.feladat: A legkisebb külömbség előfordulása: {0}", hanyszor);
+
+            bool volt = false;
+            for (int i = 0; i < g.Length; i++)
+            {
+                if (t[i].ev % 4 == 0 && t[i].honap % 2 == 0/*&& t[i].nap % 24 == 0*/)
+                {
+                    volt = true;
+                }
+            }
+            if (volt == true)
+            {
+                Console.WriteLine("Volt változás");
+            }
+            else
+            {
+                Console.WriteLine("Nem volt Változás");
+            }
+            Console.ReadKey();
+        }
+    }
+}
