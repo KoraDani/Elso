912159ff651252c2a2934f653a1015a04355e6ac
KoraDani <kora.dani@gmail.com>
2020-01-10 15:19:36 +0100

    javitva, mukodik

--- Uzemanyag/Uzemanyag/Program.cs
+++ Uzemanyag/Uzemanyag/Program.cs
@@ -22,20 +22,30 @@ namespace Uzemanyag
         {
             string[] g = File.ReadAllLines("uzemanyag.txt");
             uzem[] t = new uzem[g.Length];
-            for (int i = 0; i < g.Length; i++)
-            {
-                string[] h = g[i].Split('.');
-                t[i].ev = int.Parse(h[0]);
-                t[i].honap = int.Parse(h[1]);
-                //t[i].nap = Convert.ToInt32(h[2]);
-            }
+/*
             for (int i = 0; i < g.Length; i++)
             {
                 string[] f = g[i].Split(';');
-                t[i].benzin = Convert.ToInt32(f[0]);
-                t[i].dizel = Convert.ToInt32(f[1]);
+                for (int j = 0; j < g.Length; j++)
+                {
+                    string[] h = g[i].Split('.');
+                    t[i].ev = int.Parse(h[0]);
+                    t[i].honap = int.Parse(h[1]);
+                    t[i].nap = Convert.ToInt32(h[2]);
+                }
+                t[i].benzin = Convert.ToInt32(f[3]);
+                t[i].dizel = Convert.ToInt32(f[4]);
             }
-
+            */
+            for (int i = 0; i < g.Length; i++)
+            {
+                string[] f = g[i].Split(';');
+                t[i].ev = int.Parse(f[0].Split('.')[0]);
+                t[i].honap = int.Parse(f[0].Split('.')[1]);
+                t[i].nap = int.Parse(f[0].Split('.')[2]);
+                t[i].benzin = int.Parse(f[1]);
+                t[i].dizel = int.Parse(f[2]);
+            }
             Console.WriteLine("3.feladat: Változások száma {0}", g.Length);
 
             int min = Math.Abs(t[0].benzin - t[0].dizel);
@@ -75,6 +85,19 @@ namespace Uzemanyag
             {
                 Console.WriteLine("Nem volt Változás");
             }
+            StreamWriter ir = new StreamWriter("euro.txt");
+            for (int i = 0; i < g.Length; i++)
+            {
+                ir.WriteLine("{0}.{1}.{2} {3:0.00}Euro {4:0.00}Euro",t[i].ev,t[i].honap,t[i].nap,(t[i].benzin/307.7),(t[i].dizel/307.7));
+            }
+            ir.Close();
+
+            int beevszam;
+            do
+            {
+                Console.Write("Kérem adja meg az évszámot! [2011..2016]: ");
+                beevszam = Convert.ToInt32(Console.ReadLine());
+            } while (beevszam <= 2011 || beevszam >= 2016);
             Console.ReadKey();
         }
     }
